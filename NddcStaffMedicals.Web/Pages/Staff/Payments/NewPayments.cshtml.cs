72dba91ac245dd073c3dafe7d3d2e832a74361ee
Orchiid <joannatolofari@gmail.com>
2023-12-12 10:04:23 +0100

    new

---
+++ NddcStaffMedicals.Web/Pages/Staff/Payments/NewPayments.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NddcStaffMedicals.Web.Pages.Staff.Payments
+{
+    public class NewPaymentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
